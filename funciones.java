c2446d0aea6571111791df0955d72fbbb8048793
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 15:19:05 +0100

    Subo Funcion 1

---
+++ funciones.java
@@ -0,0 +1,18 @@
+public class Funcion1 {
+	public static void main(String[] args) {
+		System.out.println(funcion1(40));
+	}
+		
+		static int x = 5;
+		
+		public static boolean funcion1 (int n){
+
+			boolean resp;
+			if(n % 5 == 0) {
+				resp= true;
+			}else {
+				resp=false;
+			}
+			return resp;
+		};
+}
