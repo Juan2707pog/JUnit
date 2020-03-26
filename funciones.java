70a3051ab65ec6b4eb71bf8725e0a0d15eb2060e
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 15:25:21 +0100

    Subo Funcion2

--- funciones.java
+++ funciones.java
@@ -1,3 +1,6 @@
+
+package Funciones;
+
 public class Funcion1 {
 	public static void main(String[] args) {
 		System.out.println(funcion1(40));
@@ -15,4 +18,30 @@ public class Funcion1 {
 			}
 			return resp;
 		};
+}
+public class Funcion2 {
+
+        public static void main(String[] args) {
+
+                System.out.println(Funcion2(153));
+        }
+
+        public static Boolean Funcion2(int num) {
+
+                Boolean resp = null;
+
+                int y = 200 - 50;
+                int x = 200 + 50;
+
+                if (num <= x && num >= y) {
+
+                        resp = true;
+
+                } else {
+                        resp = false;
+                }
+
+                return resp;
+
+        }
 }
