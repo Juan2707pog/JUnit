7fe70521159b49e221000b5d4920c4e31df2a0b6
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 17:42:23 +0100

    Subo Funcion8

--- funciones.java
+++ funciones.java
@@ -202,3 +202,27 @@ public class Funcion7 {
             }
 }
 
+//no definimos algunos parametros importantes, como los nombres de los alumnos
+public class Funcion8 {
+	public static void main(String[] args) {
+        String [] alumnos = {"Linomenchelo","Cav1nem","Lenendi","Livlad"};
+        int [][] tiemposDeTrabajos = { {0,0,0,0} ,  {4,0,3,2}};
+        System.out.println(Funcion8(alumnos, tiemposDeTrabajos));
+    }
+public static boolean Funcion8(String[] alumnos, int[][] tiemposDeTrabajos){
+            int t = 400;
+    int sumatorio = 0;
+    boolean result = true;
+    for(int i = 0; i < tiemposDeTrabajos.length; i++){
+        sumatorio = 0;
+        for(int j = 0; j < tiemposDeTrabajos.length; j++){
+            sumatorio += tiemposDeTrabajos[j][i];
+        }
+        if(sumatorio<=t){
+            result = false;
+        }
+    }
+    return result;
+
+    }
+}
