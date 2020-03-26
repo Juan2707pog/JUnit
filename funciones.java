fc06a720f8a3a9fd703223679334e6593998e3b3
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 15:39:43 +0100

    Subo Funcion5

--- funciones.java
+++ funciones.java
@@ -90,3 +90,21 @@ public class Funcion4 {
                 return CadenaConcatenada;
             }
     }
+public class Funcion5{
+
+public static void main(String[] args) {
+
+}
+	public  int Funcion5(int [] num) {
+
+		int tam = num.length;
+		int sum = 0; 
+		int [] numarray = new int [tam];
+
+		for (int cont = 0; cont < tam; cont++) {
+			numarray[cont] = num[cont];
+			sum = sum + numarray[cont];
+		}
+		return sum;
+	}
+}
