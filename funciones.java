c27216d5a4260a8d85926f9268480d2c8af2b7fb
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 15:28:10 +0100

    Subo Funcion3

--- funciones.java
+++ funciones.java
@@ -45,3 +45,25 @@ public class Funcion2 {
 
         }
 }
+public class Funcion3 {
+    public static void main(String[] args) {
+        System.out.println(Funcion3(200));
+    }
+    public static int Funcion3(int num) {
+
+
+        int x = 5;
+        int ymenos = 200-50;
+        int ymas = 200+50;
+        int rang = (int)Math.floor(Math.random()*(ymas+ymenos));
+        int result = 0;
+
+        if (num%x == 0) {
+            result = rang%num;
+        }
+
+        return result;
+
+    }
+
+}
