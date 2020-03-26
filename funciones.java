07f0ba6f4f7df16c442cc767f4d2434cc331ea8d
juan2707pog <contacto.martinez.2707@gmail.com>
2020-03-26 15:36:35 +0100

    Subo Funcion4

--- funciones.java
+++ funciones.java
@@ -67,3 +67,26 @@ public class Funcion3 {
     }
 
 }
+public class Funcion4 {
+        public static void main(String[] args) {
+
+
+            String[] CadenaNombres = { "Carlos Villar", "Javier Melendez", "Juan Jesus", "Carlos Garcia" };
+
+            System.out.println(Funcion4(CadenaNombres));
+
+            }
+            public static String Funcion4(String[] CadenaNombres) {
+
+                String CadenaConcatenada = null;
+                for (int i = 0; i < CadenaNombres.length; i++) {
+
+                    CadenaConcatenada = CadenaNombres[0] + "-" + CadenaNombres[1] + "-" + CadenaNombres[2] + "-"
+                            + CadenaNombres[3];
+                }
+                CadenaConcatenada = CadenaConcatenada.replaceAll("[AaEeIiOoUu]", "");
+                CadenaConcatenada = CadenaConcatenada.replaceAll("[A-Z]", "");
+                System.out.println(CadenaConcatenada);
+                return CadenaConcatenada;
+            }
+    }
